848fe9f76179d3b1cc7c35bb20db517b800bb466
Egor <stroev776@gmail.com>
2025-03-20 21:51:46 +0300

    Added Many Files

--- Lesson_5.py
+++ Lesson_5.py
@@ -2,5 +2,11 @@ from termcolor import cprint
 from random import randint as Generate_Number 
 from Utils.Calculator import division
 from Utils.Person import Student
+from decouple import config
 MyFr = Student(20, "Kevin")
 cprint("Hi", "green")
+
+print(config("DATABASE_URL"))
+
+commented = config("COMMENTED", default=0)
+print(commented * 2)
